a02b7ced45f15dfa10a7948bf913d106c17943a6
Miguel Brown <browncoloma@gmail.com>
2022-09-05 01:01:52 -0400

    se agrega wouter y se trabaja con route y link

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import ListOfGif from './components/ListOfGif';
-import getGifs from './services/getGifs';
+import { Link, Route } from "wouter";
 
-
-
-function App() {
-
-  const [gifs, setGifs] = useState([])
-  
-  useEffect(function (){
-    console.log('actualizando gifs')
-    // setGifs(DIFFERENT_GIFS)
-    getGifs({keyword:'Chile'}).then(gifs=>setGifs(gifs))
-  }, [])
-  
-
+export default function App() {
   return (
     <div className="App">
       <section className="App-content">
-        <ListOfGif gifs={gifs}/>
+        <h1>App</h1>
+        <Link to='/gif/matrix'>Gif de Matrix</Link>
+        <Link to='/gif/caballos'>Gif de Caballos</Link>
+        <Link to='/gif/ricky and morty'>Gif de Riqki and Morty</Link>
+        <Route 
+          path='/gif/:keyword' component={ListOfGif}/>
+        {/* <button onClick={()=> setKeyword('matrix 2')}>Cambio de keyword</button> */}
+        {/* <ListOfGif keyword={keyword}/> */}
         {/* <button onClick={() => setGifs(DIFFERENT_GIFS)}>Cambiar Gifs</button> */}
       </section>
     </div>
   );
 }
-
-export default App;
--- src/components/ListOfGif.js
+++ src/components/ListOfGif.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Gif from "./Gif";
+import getGifs from "../services/getGifs";
 
-export default function ListOfGif ({gifs}) {
-  return gifs.map(({ id, title, url }) =>
-    <Gif 
-      key={id} 
-      title={title} 
-      url={url} 
-      id={id} 
-    />
-  )
+export default function ListOfGif({ params }) {
+  const {keyword}=params
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(
+    function () {
+      console.log("actualizando gifs");
+      // setGifs(DIFFERENT_GIFS)
+      getGifs({ keyword }).then((gifs) => setGifs(gifs));
+    },
+    [keyword]
+  );
+  return (
+    <div>
+      {gifs.map(({ id, title, url }) => (
+        <Gif key={id} title={title} url={url} id={id} />
+      ))}
+    </div>
+  );
 }
